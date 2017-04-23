312eca4de23c9456b004710955265018e49a02d6
Johannes Moskvil <johannesmos@hotmail.com>
2017-04-23 12:46:41 +0000

    init commit

---
+++ background_subtractor.py
@@ -0,0 +1,161 @@
+import cv2
+import time
+import imutils
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import json
+import os
+import inspect
+
+class BackgroundExtractor:
+
+    def __init__(self, firstFrame, conf, path_to_file):
+        frame = imutils.resize(firstFrame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        self.avg = gray.copy().astype("float")
+        self.current_frame = self.avg
+        self.previous_positive_detection = time.time()
+
+        self.negative_seconds_limit = conf["consecutive_negative_seconds_limit"]
+        self.min_area = conf["min_area"]
+        self.show_feed = conf["show_video"]
+        self.is_dynamic = conf["dynamic_background"]
+        self.face_cascade = cv2.CascadeClassifier(os.path.join(path_to_file, conf["cascade_path"]))
+	print os.path.join(path_to_file, conf["cascade_path"])
+	
+
+    def getPotentialRegions(self, frame):
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        self.current_frame = gray
+
+        if self.show_feed["blur"]:
+            cv2.imshow('Blurred', gray)
+
+        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))
+
+        if self.show_feed["average"]:
+            cv2.imshow('Average', cv2.convertScaleAbs(self.avg))
+
+        thresh = cv2.threshold(frameDelta, 15, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cv2.imshow("Countor", thresh)
+
+        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                     cv2.CHAIN_APPROX_SIMPLE)
+
+        areas = [x for x in cnts if cv2.contourArea(x) > self.min_area]
+        
+        cropped_images = []
+        for a in areas:
+            x, y, w, h = cv2.boundingRect(a)
+            cropped_images.append((frame[y:y + h, x:x + w], (x, y, w, h)))
+
+            if self.show_feed["contour"]:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                cv2.imshow('Video CONTOUR', frame)
+
+        return cropped_images
+
+    def getBoundingBox(self, frame):
+        potentialAreas = self.getPotentialRegions(frame)
+        bounding_boxes = []
+
+        for area, bounding_box in potentialAreas:
+            faces = self.face_cascade.detectMultiScale(
+                area,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+            )
+            #update background model if noe faces found
+            if len(faces) == 0 and self.is_dynamic:
+                self.accumulate_background()
+            
+            bounding_boxes.extend(self.get_face_bounds(faces, bounding_box))
+            
+        return bounding_boxes
+
+    def accumulate_background(self):
+        if time.time()-self.previous_positive_detection > self.negative_seconds_limit:
+            cv2.accumulateWeighted(self.current_frame, self.avg, 0.01)
+            #print "updating"
+            #self.avg = self.current_frame
+
+
+    def get_face_bounds(self, faces, bounding_box):
+        bounding_boxes = []
+        for face in faces:
+            (x1, y1, w1, h1) = face
+            x, y, w, h = bounding_box
+            top = y+y1
+            right = x+x1+w1
+            bottom = y+y1+h1
+            left = x+x1
+            bounding_boxes.append((top, right, bottom, left))
+            self.previous_positive_detection = time.time()
+        return bounding_boxes
+
+
+
+if __name__ == "__main__":
+    #video_capture = cv2.VideoCapture(0)
+    conf = json.load(open('conf.json'))
+    path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    video_capture = PiCamera()
+    video_capture.resolution = (640, 480)
+    video_capture.framerate = 16
+    raw_capture = PiRGBArray(video_capture, size=(640,480))
+    time.sleep(2.0)
+    video_capture.capture(raw_capture, format="bgr")
+    bgsub = BackgroundExtractor(raw_capture.array, conf, path_to_file)
+    face_cascade = cv2.CascadeClassifier('cascades/lbpcascade_frontalface.xml')
+    debug=True
+    raw_capture.truncate(0)
+    try:
+        for f in video_capture.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+            frame = f.array
+            frame = imutils.resize(frame, width=500)
+
+            potentialAreas = bgsub.getPotentialRegions(frame)
+
+            if debug:
+                regions = bgsub.getBoundingBox(frame)
+                for x,y,w,h in regions:
+                    cv2.rectangle(frame, (h, x), (y, w), (100, 0, 100), 2)
+
+                print "negative frames count: " + str(time.time()-bgsub.previous_positive_detection)
+
+
+
+            all_faces = []
+            for area, bounding_box in potentialAreas:
+                faces = face_cascade.detectMultiScale(
+                    area,
+                    scaleFactor=1.1,
+                    minNeighbors=5,
+                    minSize=(30, 30),
+                    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+                )
+                for face in faces:
+		    print "face found"
+                    (x1, y1, w1, h1) = face
+                    x, y, w, h = bounding_box
+                    cv2.rectangle(frame, (x+x1, y+y1), (x+x1 + w1, y+y1 + h1), (0, 255, 0), 2)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+                cv2.imshow('Face detection', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            raw_capture.truncate(0)
+    except KeyboardInterrupt:
+        pass
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
---
+++ deepnet_recognition.py
@@ -0,0 +1,178 @@
+import face_recognition
+import cv2
+import time
+import background_subtractor as bgsub
+import imutils
+import json
+import sys
+import inspect
+import os
+import signal
+import camera
+
+def to_node(type, message):
+    # convert to json and print (node helper will read from stdout)
+    try:
+        print(json.dumps({type: message}))
+    except Exception:
+        pass
+    # stdout has to be flushed manually to prevent delays in the node helper communication
+    sys.stdout.flush()
+
+
+def shutdown(self, signum):
+    to_node("status", 'Shutdown: Cleaning up camera...')
+    camera.stop()
+    quit()
+
+signal.signal(signal.SIGINT, shutdown)
+
+path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+conf = json.load(open(path_to_file + '/conf.json'))
+
+camera = camera.get_camera(False)
+time.sleep(conf["camera_warmup_time"])
+
+# Load a sample picture and learn how to recognize it.
+johannes_image = face_recognition.load_image_file(path_to_file + "/images/johannes.jpg")
+mathias_image = face_recognition.load_image_file(path_to_file + "/images/mathias.jpg")
+ingunn_image = face_recognition.load_image_file(path_to_file + "/images/ingunn.jpg")
+jessie_image = face_recognition.load_image_file(path_to_file + "/images/jessie.jpg")
+
+johannes_face_encoding = face_recognition.face_encodings(johannes_image)[0]
+mathias_face_encoding = face_recognition.face_encodings(mathias_image)[0]
+ingunn_face_encoding = face_recognition.face_encodings(ingunn_image)[0]
+jessie_face_encoding = face_recognition.face_encodings(jessie_image)[0]
+faces = [johannes_face_encoding, mathias_face_encoding, ingunn_face_encoding, jessie_face_encoding]
+
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+bg_sub_model = None
+performance_stats = {}
+time_since_Known_face = 0
+current_user = None
+recent_detections = [-1]*conf["consecutive_detections"]
+detection_index = 0
+
+
+def get_face_locations_dlib(frame):
+    return face_recognition.face_locations(frame)
+
+
+def get_face_locations_bgsub(frame):
+    return bg_sub_model.getBoundingBox(frame)
+
+
+def get_face_encoding(frame, face_locations, debug=True):
+    time_start = time.time()
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    if debug:
+        time_end = time.time()
+        performance_stats["Encoding"] = time_end-time_start
+
+    return face_encodings
+
+
+def get_face_locations(frame, debug=True):
+    time_start = time.time()
+    face_locations = get_face_locations_bgsub(frame)
+
+    if debug:
+        time_end = time.time()
+        performance_stats["Detection"] = time_end-time_start
+
+    return face_locations
+
+
+def get_face_match(faces, face_encoding, debug=True):
+    time_start = time.time()
+    match = face_recognition.compare_faces(faces, face_encoding)
+
+    if debug:
+        time_end = time.time()
+        performance_stats["Matching"] = time_end-time_start
+
+    return match
+
+
+def update_detection_list(index):
+    global detection_index
+    recent_detections[detection_index] = index
+    detection_index = (detection_index + 1) % conf["consecutive_detections"]
+
+
+def check_login():
+    global recent_detections, current_user
+    if recent_detections[0] != -1 and recent_detections[1:] == recent_detections[:-1] and current_user != recent_detections[0]:
+        current_user = recent_detections[0]
+        to_node("login", {"user": current_user+1})
+
+
+def check_logout():
+    global current_user, recent_detections
+    if time.time() - time_since_Known_face > conf["logout_time"] and current_user is not None:
+        to_node("logout", {"user": current_user+1})
+        recent_detections = [-1]*conf["consecutive_detections"]
+        current_user = None
+
+
+while True:
+    # Grab a single frame of video
+    frame = camera.read()
+    frame = imutils.resize(frame, width=500)
+
+    if bg_sub_model is None:
+        bg_sub_model = bgsub.BackgroundExtractor(frame, conf, path_to_file)
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        face_locations = get_face_locations(frame)
+        face_encodings = get_face_encoding(frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            match = get_face_match(faces, face_encoding)
+
+            name, index = "Unknown", -1
+
+            for i in range(len(match)):
+                if match[i]:
+                    time_since_Known_face = time.time()
+                    index = i
+
+            update_detection_list(index)
+            face_names.append(conf["users"][index])
+            
+        check_login()
+        check_logout()
+
+    process_this_frame = not process_this_frame
+
+    if conf["show_video"]["recognition"]:
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+            cv2.putText(frame, "Detection: " + str(round(performance_stats["Detection"], 5)), (10, 20), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(frame, "Encoding: " + str(round(performance_stats["Encoding"], 5)), (10, 40), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(frame, "Matching: " + str(round(performance_stats["Matching"], 5)), (10, 60), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Release handle to the webcam
+#video_capture.release()
+#cv2.destroyAllWindows()
